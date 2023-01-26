a49675ebcd74b6975b75c9fe653c02c3ef086c12
Luciano Mariano <65924740+lucianoafmariano@users.noreply.github.com>
2023-01-26 04:07:42 -0300

    Add files via upload

---
+++ site_teste/scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "imagens/BREW-3198.jpg") {
+    myImage.setAttribute("src", "imagens/BREW-3263.jpg");
+  } else {
+    myImage.setAttribute("src", "imagens/BREW-3198.jpg");
+  }
+};
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
